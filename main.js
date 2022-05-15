25b05866da5f933fe7f4844be3c90310a7cd7eb5
Kevin "d415uk3" Wallen <46876927+daisukedd@users.noreply.github.com>
2022-05-15 19:07:01 -0300

    Stage 5
    CSS Media Queries
    CSS Flex Avançado

--- main.js
+++ main.js
@@ -5,7 +5,37 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+
+    activeMenuAtCurrentSection(home)
+    activeMenuAtCurrentSection(services)
+    activeMenuAtCurrentSection(about)
+    activeMenuAtCurrentSection(contact)
 }
+
+function activeMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    const sectionEndsPassedTargetLine = sectionEndsAt <= targetLine
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndsPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
+}
+
+
 
 function showNavOnScroll() {
     if (scrollY > 0) {
